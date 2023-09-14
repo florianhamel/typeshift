d732ef00efcc1d3bf631789551f7734e17b11077
Florian Hamel <fhamel.freelance@gmail.com>
2023-09-14 21:45:15 +0200

    TSW-08: upgraded ux + added wikimedia api extracts

---
+++ front/src/app/components/typing/typing/typing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-typing',
+  templateUrl: './typing.component.html',
+  styleUrls: ['./typing.component.less']
+})
+export class TypingComponent {
+}
